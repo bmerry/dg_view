c92e48444fab01f83c36378df77f6a0a1dbc1090
Bruce Merry <bmerry@users.sourceforge.net>
2010-09-15 20:09:38 +0100

    Import dg_view from tag datagrind-20100915 (g966a81a) from Valgrind repo

---
+++ src/dg_record.h
@@ -0,0 +1,48 @@
+/*
+   This file is part of Datagrind, a tool for tracking data accesses.
+   It borrows heavily from the Lackey tool.
+
+   Copyright (C) 2010 Bruce Merry
+      bmerry@users.sourceforge.net
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307, USA.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+#ifndef __DG_RECORD_H
+#define __DG_RECORD_H
+
+#define DG_R_HEADER           0
+#define DG_R_READ             1
+#define DG_R_WRITE            2
+#define DG_R_TRACK_RANGE      3
+#define DG_R_UNTRACK_RANGE    4
+#define DG_R_START_EVENT      5
+#define DG_R_END_EVENT        6
+#define DG_R_INSTR            7
+#define DG_R_TEXT_AVMA        8
+#define DG_R_MALLOC_BLOCK     9
+#define DG_R_FREE_BLOCK      10
+#define DG_R_BBDEF           11
+#define DG_R_BBRUN           12
+#define DG_R_CONTEXT         13
+
+#define DG_ACC_READ           0
+#define DG_ACC_WRITE          1
+#define DG_ACC_EXEC           2
+
+#endif /* __DG_RECORD_H */
